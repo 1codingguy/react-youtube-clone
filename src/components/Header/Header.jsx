9b652584ac825a8804b76783b2c013ec7f7cd607
coding-guy <1codingguy@gmail.com>
2021-08-26 12:27:16 +0700

    hide AppBar when scroll passed the element

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,31 +5,8 @@ import Toolbar from '@material-ui/core/Toolbar'
 import SearchIcon from '@material-ui/icons/Search'
 import Avatar from '@material-ui/core/Avatar'
 import IconButton from '@material-ui/core/IconButton'
-
-export default function Header() {
-  return (
-    <>
-      <StyledAppBar>
-        <Toolbar>
-          <YouTubeLogo
-            src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
-            alt="logo"
-          />
-
-          <ToolbarRight>
-            <IconButton >
-              <SearchIcon />
-            </IconButton>
-
-            <IconButton>
-              <Avatar>C</Avatar>
-            </IconButton>
-          </ToolbarRight>
-        </Toolbar>
-      </StyledAppBar>
-    </>
-  )
-}
+import useScrollTrigger from '@material-ui/core/useScrollTrigger'
+import Slide from '@material-ui/core/Slide'
 
 const ToolbarRight = styled.div`
   flex-grow: 1;
@@ -70,3 +47,51 @@ const StyledAppBar = styled(AppBar)`
     padding-right: 0;
   }
 `
+// modified from example: https://material-ui.com/components/app-bar/
+function HideOnScroll({ children }) {
+  const trigger = useScrollTrigger({ threshold: '48' })
+  return (
+    <Slide in={!trigger}>
+      {/* previously added wrap a <div> around {children, the hide animation didn't work as a result} */}
+      {children}
+    </Slide>
+  )
+}
+
+export default function Header() {
+  return (
+    <>
+      <HideOnScroll>
+        <StyledAppBar>
+          <Toolbar>
+            <YouTubeLogo
+              src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
+              alt="logo"
+            />
+
+            <ToolbarRight>
+              <IconButton>
+                <SearchIcon />
+              </IconButton>
+
+              <IconButton>
+                <Avatar>C</Avatar>
+              </IconButton>
+            </ToolbarRight>
+          </Toolbar>
+        </StyledAppBar>
+      </HideOnScroll>
+
+      <div>
+        {[...new Array(12)]
+          .map(
+            () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+          )
+          .join('\n')}
+      </div>
+    </>
+  )
+}
