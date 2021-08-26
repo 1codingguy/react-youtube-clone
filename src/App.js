da0da56b62bac27e189be3a2692ed3e2625aa110
coding-guy <1codingguy@gmail.com>
2021-08-26 11:45:52 +0700

    change extension form Header.js to Header.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Header from "./components/Header/Header";
+import Header from './components/Header/Header.jsx'
 
 function App() {
   return (
     <div className="App">
       <Header />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
